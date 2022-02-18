65136f1a498e4a94367f48e2b5a54f6efa6aeccf
paolo <paolonazzaro23@gmail.com>
2022-02-18 09:25:58 +0100

    _redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SerieAComponent } from './serie-a/serie-a.component';
+import { NbaComponent } from './nba/nba.component';
+import { HeaderComponent } from './header/header.component';
+import { DialogComponent } from './dialog/dialog.component';
 
+// MATERIAL
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-
-import { DialogComponent } from './dialog/dialog.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { MatButtonModule } from '@angular/material/button';
-import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
-import { HeaderComponent } from './header/header.component';
-import { NbaComponent } from './nba/nba.component';
-import { SerieAComponent } from './serie-a/serie-a.component';
 import { MatRippleModule } from '@angular/material/core';
+
+// ANALYTICS
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,8 @@ import { MatRippleModule } from '@angular/material/core';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // Material modules
+    NgxGoogleAnalyticsModule.forRoot('G-Z7KXKM6680'),
+  // MATERIAL
     MatCardModule,
     MatDialogModule,
     MatButtonModule,
@@ -41,7 +45,7 @@ import { MatRippleModule } from '@angular/material/core';
     MatMenuModule,
     MatRippleModule,
     BrowserAnimationsModule,
-    NgxGoogleAnalyticsModule.forRoot('G-Z7KXKM6680'),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
